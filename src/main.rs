9e7bbafbdb49060f2636fd622dd2569595adb228
Jonah Beston <jonah.beston@gmail.com>
2025-08-26 20:55:03 -0400

    Seperated subcommands
    Subcommands are now within their own file

--- src/main.rs
+++ src/main.rs
@@ -3,180 +3,34 @@ use std::io::{self, Write};
 use termion;
 
 mod data_file;
-    use data_file::{DataFile, ClassData};
+    use data_file::DataFile;
 mod error;
     use error::Error;
+mod subcommands;
 
 
 /// Manager for the application's Text User Interface
-struct TUI {
-    data_file: DataFile,
-} impl TUI {
+struct TUI {}
+impl TUI {
     /// Create new instance of this
-    pub fn new() -> Self { return Self {
-        data_file: DataFile::get(),
-    }}
+    pub fn new() -> Self { return Self {}}
 
     /// Display the current state of the app to `Stdout`
-    pub fn display(&self) {
-        for (class_name, class_data) in self.data_file.content.iter() {
+    pub fn display(&self, data_file: &DataFile) {
+        for (class_name, class_data) in data_file.content.iter() {
             println!("[{}] {}:", class_data.tag, class_name);
             for (assignment_index, assignment) in class_data.assignments.iter().enumerate() {
                 println!("    {}) {}", assignment_index + 1, assignment);
             }
+            println!();
         }
     }
-}
-
-/// Prints a help message
-fn help() {
-    println!(
-r#"
-ac <class_name> <class_tag>
-    Add a class
-
-rc <class_tag>
-    Remove a class
-
-cc <class_tag> <new_class_tag>
-    Change a classes tag
-
-mc <class_tag> <class_position>
-    Move a class up [u] or down [d]
-
-aa <class_tag> <assignment>
-    Add an assignment to a class
-
-ra <class_tag> <assignment_index>
-    Remove an assignment from a class
-
-ca <class_tag>
-    Clear a classes assignments
-"#
-    );
-
-    io::stdout().flush().unwrap();
-
-    let mut input_buf = String::new();
-    io::stdin().read_line(&mut input_buf).unwrap();
-}
-
-/// Command to add a class
-fn add_class(data_file: &mut DataFile, class_name: &str, class_tag: &str) {
-    // Insert the new class
-    let class_metadata = ClassData::new(class_tag, []);
-    data_file.content.insert(class_name.into(), class_metadata);
-
-    // Write to the data file
-    data_file.write();
-}
-
-/// Command to remove a class
-fn remove_class(data_file: &mut DataFile, class_tag: &str) {
-    // Remove given class
-    if let Some(class_index) = data_file.clone().find_class_index_from_tag(class_tag) {
-        data_file.content.swap_remove_index(class_index);
-    } else {
-        Error::ClassOfThatTagNotFound.show();
-    }
-
-    // Write to the data file
-    data_file.write();
-}
-
-/// Command to change a classes tag
-fn change_class_tag(data_file: &mut DataFile, from_class_tag: &str, to_class_tag: &str) {
-    // Check if the new tag already exists
-    if data_file.clone().tag_already_exists(to_class_tag) { Error::TagAleadyExists.show() }
-
-    // Find the old tag and replace it
-    if let Some((_, class_tag)) = data_file.find_class_from_tag(from_class_tag) {
-        class_tag.tag = to_class_tag.to_owned();
-    } else {
-        Error::ClassOfThatTagNotFound.show();
-    }
-
-    // Write to the data file
-    data_file.write();
-}
-
-/// Command to move a class up or down
-fn move_class(data_file: &mut DataFile, class_tag: &str, position: &str) {
-    if let Some(class_index) = data_file.clone().find_class_index_from_tag(class_tag) {
-        if position == "u" && class_index < data_file.clone().content.len() - 1 {
-            data_file.content.move_index(class_index, class_index + 1);
-        } else if position == "d" && class_index > 0 {
-            data_file.content.move_index(class_index, class_index - 1);
-        } else {
-            Error::InvalidMovementDirection.show();
-            return;
-        }
-    } else {
-        Error::ClassOfThatTagNotFound.show();
-        return;
-    }
-
-    // Write to the data file
-    data_file.write();
-}
-
-/// Command to add an assignment to a specified class
-fn add_assignment(data_file: &mut DataFile, class_tag: &str, assignment_name: &str) {
-    // Add the assignment
-    if let Some((_, class_data)) = data_file.find_class_from_tag(class_tag) {
-        class_data.assignments.push(assignment_name.to_owned());
-    } else {
-        Error::ClassOfThatTagNotFound.show();
-        return;
-    }
-
-    // Write to the data file
-    data_file.write();
-}
-
-/// Command to remove an assignment from a specified class by its index
-fn remove_assignment(data_file: &mut DataFile, class_tag: &str, assignment_index: usize) {
-    println!("{}", assignment_index);
-
-    // Remove given assignment by index
-    if let Some((_, class_data)) = data_file.find_class_from_tag(class_tag) {
-        // get the number of assignments
-        let assignment_len = class_data.clone().assignments.len();
-
-        // remove the assignment if the index is valid
-        let actual_assignment_index: isize = assignment_index as isize - 1;
-        if actual_assignment_index >= 0 && actual_assignment_index < assignment_len as isize {
-            _ = class_data.assignments.remove(actual_assignment_index as usize);
-        } else {
-            Error::InvalidAssignmentIndex.show();
-            return;
-        }
-    } else {
-        Error::ClassOfThatTagNotFound.show();
-        return;
-    }
-
-    // Write to the data file
-    data_file.write();
-}
-
-/// Command to clear assignments in a specified class
-fn clear_assignments(data_file: &mut DataFile, class_tag: &str) {
-    // Clear assignments
-    if let Some((_, class_data)) = data_file.find_class_from_tag(class_tag) {
-        class_data.assignments.clear();
-    } else {
-        Error::ClassOfThatTagNotFound.show();
-        return;
-    }
-
-    // Write to the data file
-    data_file.write();
 }
 
 
 fn main() {
-    let mut tui = TUI::new();
+    let tui = TUI::new();
+    let mut data_file = DataFile::get();
 
     let mut is_running = true;
     while is_running {
@@ -185,7 +39,7 @@ fn main() {
         println!("{}", termion::cursor::Goto(1, 1));
 
         // Display the TUI
-        tui.display();
+        tui.display(&data_file);
 
         // Get the given command
         print!("> ");
@@ -195,57 +49,58 @@ fn main() {
         let input_parse: Vec<&str> = user_input.split(" ").map(|x| x.trim()).collect();
 
         // Match the command
-        match input_parse.clone()[0].trim() {
+        let subcommand = input_parse.clone()[0].trim();
+        match subcommand {
             "e" | "exit" => is_running = false,
-            "h" | "help" => help(),
+            "h" | "help" => subcommands::help(),
 
-            "ac" => {
+            _ if subcommand == subcommands::ADD_CLASS_COMMAND => {
                 if input_parse.len() != 3 {
                     Error::MissingArguments.show();
                     continue;
                 }
 
-                add_class(&mut tui.data_file, &input_parse[1], &input_parse[2]);
+                subcommands::add_class(&mut data_file, &input_parse[1], &input_parse[2]);
             }
 
-            "rc" => {
+            _ if subcommand == subcommands::REMOVE_CLASS_COMMAND => {
                 if input_parse.len() != 2 {
                     Error::MissingArguments.show();
                     continue;
                 }
 
-                remove_class(&mut tui.data_file, &input_parse[1]);
+                subcommands::remove_class(&mut data_file, &input_parse[1]);
             }
 
-            "cc" => {
+            _ if subcommand == subcommands::CHANGE_CLASS_TAG_COMMAND => {
                 if input_parse.len() != 3 {
                     Error::MissingArguments.show();
                     continue;
                 }
 
-                change_class_tag(&mut tui.data_file, &input_parse[1], &input_parse[2]);
+                subcommands::change_class_tag(&mut data_file, &input_parse[1], &input_parse[2]);
             }
 
-            "mc" => {
+            _ if subcommand == subcommands::MOVE_CLASS_COMMAND => {
                 if input_parse.len() != 3 {
                     Error::MissingArguments.show();
                     continue;
                 }
 
-                move_class(&mut tui.data_file, &input_parse[1], &input_parse[2])
+                subcommands::move_class(&mut data_file, &input_parse[1], &input_parse[2])
             }
 
-            "aa" => {
+            _ if subcommand == subcommands::ADD_ASSIGNMENT_COMMAND => {
                 if input_parse.len() < 3 {
                     Error::MissingArguments.show();
                     continue;
                 }
 
                 let assignment_name: String = input_parse.clone()[2..].join(" ");
-                add_assignment(&mut tui.data_file, &input_parse[1], &assignment_name);
+                subcommands::add_assignment(&mut data_file, &input_parse[1], &assignment_name);
             }
 
-            "ra" => {
+            _ if subcommand == subcommands::REMOVE_ASSIGNMENT_COMMAND => {
                 if input_parse.len() != 3 {
                     Error::MissingArguments.show();
                     continue;
@@ -253,20 +108,20 @@ fn main() {
 
                 let assignment_index = input_parse.clone()[2].parse::<usize>();
                 if let Ok(assignment_index) = assignment_index {
-                    remove_assignment(&mut tui.data_file, &input_parse[1], assignment_index);
+                    subcommands::remove_assignment(&mut data_file, &input_parse[1], assignment_index);
                 } else {
                     Error::InvalidArgumentType.show();
                     continue;
                 }
             }
 
-            "ca" => {
+            _ if subcommand == subcommands::CLEAR_ASSIGNMENTS_COMMAND => {
                 if input_parse.len() != 2 {
                     Error::MissingArguments.show();
                     continue;
                 }
 
-                clear_assignments(&mut tui.data_file, &input_parse[1]);
+                subcommands::clear_assignments(&mut data_file, &input_parse[1]);
             }
 
             _ => {}
---
+++ src/subcommands.rs
@@ -0,0 +1,171 @@
+use crate::{
+    data_file::{
+        DataFile,
+        ClassData
+    },
+    error::Error,
+};
+
+use std::io::{
+    self,
+    Write,
+};
+
+
+/// Prints a help message
+pub fn help() {
+    println!(
+r#"
+{} <class_name> <class_tag>
+    Add a class
+
+{} <class_tag>
+    Remove a class
+
+{} <class_tag> <new_class_tag>
+    Change a classes tag
+
+{} <class_tag> <class_position>
+    Move a class up [u] or down [d]
+
+{} <class_tag> <assignment>
+    Add an assignment to a class
+
+{} <class_tag> <assignment_index>
+    Remove an assignment from a class
+
+{} <class_tag>
+    Clear a classes assignments
+"#, ADD_CLASS_COMMAND, REMOVE_CLASS_COMMAND, CHANGE_CLASS_TAG_COMMAND, MOVE_CLASS_COMMAND, ADD_ASSIGNMENT_COMMAND, REMOVE_ASSIGNMENT_COMMAND, CLEAR_ASSIGNMENTS_COMMAND,
+    );
+
+    io::stdout().flush().unwrap();
+
+    let mut input_buf = String::new();
+    io::stdin().read_line(&mut input_buf).unwrap();
+}
+
+/// Command to add a class
+pub const ADD_CLASS_COMMAND: &str = "ac";
+pub fn add_class(data_file: &mut DataFile, class_name: &str, class_tag: &str) {
+    // Insert the new class
+    let class_metadata = ClassData::new(class_tag, []);
+    data_file.content.insert(class_name.into(), class_metadata);
+
+    // Write to the data file
+    data_file.write();
+}
+
+/// Command to remove a class
+pub const REMOVE_CLASS_COMMAND: &str = "rc";
+pub fn remove_class(data_file: &mut DataFile, class_tag: &str) {
+    // Remove given class
+    if let Some(class_index) = data_file.clone().find_class_index_from_tag(class_tag) {
+        data_file.content.swap_remove_index(class_index);
+    } else {
+        Error::ClassOfThatTagNotFound.show();
+        return;
+    }
+
+    // Write to the data file
+    data_file.write();
+}
+
+/// Command to change a classes tag
+pub const CHANGE_CLASS_TAG_COMMAND: &str = "cc";
+pub fn change_class_tag(data_file: &mut DataFile, from_class_tag: &str, to_class_tag: &str) {
+    // Check if the new tag already exists
+    if data_file.clone().tag_already_exists(to_class_tag) {
+        Error::TagAleadyExists.show();
+        return;
+    }
+
+    // Find the old tag and replace it
+    if let Some((_, class_tag)) = data_file.find_class_from_tag(from_class_tag) {
+        class_tag.tag = to_class_tag.to_owned();
+    } else {
+        Error::ClassOfThatTagNotFound.show();
+        return;
+    }
+
+    // Write to the data file
+    data_file.write();
+}
+
+/// Command to move a class up or down
+pub const MOVE_CLASS_COMMAND: &str = "mc";
+pub fn move_class(data_file: &mut DataFile, class_tag: &str, position: &str) {
+    if let Some(class_index) = data_file.clone().find_class_index_from_tag(class_tag) {
+        if position == "u" && class_index < data_file.clone().content.len() - 1 {
+            data_file.content.move_index(class_index, class_index + 1);
+        } else if position == "d" && class_index > 0 {
+            data_file.content.move_index(class_index, class_index - 1);
+        } else {
+            Error::InvalidMovementDirection.show();
+            return;
+        }
+    } else {
+        Error::ClassOfThatTagNotFound.show();
+        return;
+    }
+
+    // Write to the data file
+    data_file.write();
+}
+
+/// Command to add an assignment to a specified class
+pub const ADD_ASSIGNMENT_COMMAND: &str = "aa";
+pub fn add_assignment(data_file: &mut DataFile, class_tag: &str, assignment_name: &str) {
+    // Add the assignment
+    if let Some((_, class_data)) = data_file.find_class_from_tag(class_tag) {
+        class_data.assignments.push(assignment_name.to_owned());
+    } else {
+        Error::ClassOfThatTagNotFound.show();
+        return;
+    }
+
+    // Write to the data file
+    data_file.write();
+}
+
+/// Command to remove an assignment from a specified class by its index
+pub const REMOVE_ASSIGNMENT_COMMAND: &str = "ra";
+pub fn remove_assignment(data_file: &mut DataFile, class_tag: &str, assignment_index: usize) {
+    println!("{}", assignment_index);
+
+    // Remove given assignment by index
+    if let Some((_, class_data)) = data_file.find_class_from_tag(class_tag) {
+        // get the number of assignments
+        let assignment_len = class_data.clone().assignments.len();
+
+        // remove the assignment if the index is valid
+        let actual_assignment_index: isize = assignment_index as isize - 1;
+        if actual_assignment_index >= 0 && actual_assignment_index < assignment_len as isize {
+            _ = class_data.assignments.remove(actual_assignment_index as usize);
+        } else {
+            Error::InvalidAssignmentIndex.show();
+            return;
+        }
+    } else {
+        Error::ClassOfThatTagNotFound.show();
+        return;
+    }
+
+    // Write to the data file
+    data_file.write();
+}
+
+/// Command to clear assignments in a specified class
+pub const CLEAR_ASSIGNMENTS_COMMAND: &str = "ca";
+pub fn clear_assignments(data_file: &mut DataFile, class_tag: &str) {
+    // Clear assignments
+    if let Some((_, class_data)) = data_file.find_class_from_tag(class_tag) {
+        class_data.assignments.clear();
+    } else {
+        Error::ClassOfThatTagNotFound.show();
+        return;
+    }
+
+    // Write to the data file
+    data_file.write();
+}
